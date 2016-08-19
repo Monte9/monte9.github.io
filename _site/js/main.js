b16f3e07f762c106c0453f8b042269a206e75920
Monte9 <Monte9@users.noreply.github.com>
2016-08-19 15:40:05 -0700

    added spacex animation for bg; working full-screen and blog toggle

--- _site/js/main.js
+++ _site/js/main.js
@@ -1,6 +1,25 @@
 $(document).ready(function () {
+
+  $('a.full-screen-button').click(function (e) {
+    var $el = $('.panel-cover[style*="max-width"]');
+    if ($el.length === 0) return
+    currentWidth = $('.panel-cover').width()
+    if (currentWidth < 960) {
+      $('.panel-cover').addClass('animated slideInLeft')
+      $('.panel-cover[style]').removeAttr('style');
+      $('.panel-cover').addClass('panel-cover')
+      $('.panel-cover').css({
+        'background-image': 'url(/images/spacex.gif'
+      });
+    } else {
+      $('.panel-cover').css('max-width', currentWidth)
+      $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {})
+    }
+  })
+
   $('a.blog-button').click(function (e) {
-    if ($('.panel-cover').hasClass('panel-cover--collapsed')) return
+    var $el = $('.panel-cover[style*="max-width"]');
+    if ($('.panel-cover').hasClass('panel-cover--collapsed') || $el.length === 1) return
     currentWidth = $('.panel-cover').width()
     if (currentWidth < 960) {
       $('.panel-cover').addClass('panel-cover--collapsed')
@@ -11,12 +30,27 @@ $(document).ready(function () {
     }
   })
 
-  if (window.location.hash && window.location.hash == '#portfolio') {
-    $('.panel-cover').addClass('panel-cover--collapsed')
-  }
-
-  if (window.location.pathname !== '/' && window.location.pathname !== '/index.html') {
-    $('.panel-cover').addClass('panel-cover--collapsed')
+  if (window.location.hash !== '#full-screen') {
+    var $el = $('.panel-cover[style*="max-width"]');
+    if ($('.panel-cover').hasClass('panel-cover--collapsed') || $el.length === 1) return
+    currentWidth = $('.panel-cover').width()
+    if (currentWidth < 960) {
+      $('.panel-cover').addClass('panel-cover--collapsed')
+      $('.content-wrapper').addClass('animated slideInRight')
+    } else {
+      $('.panel-cover').css('max-width', currentWidth)
+      $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {})
+    }
+  } else {
+    if ($('.panel-cover').hasClass('panel-cover')) return
+    currentWidth = $('.panel-cover').width()
+    if (currentWidth < 960) {
+      $('.panel-cover').addClass('panel-cover')
+      $('.content-wrapper').addClass('animated slideInLeft')
+    } else {
+      $('.panel-cover').css('max-width', currentWidth)
+      $('.panel-cover').animate({'max-width': '530px', 'width': '40%'}, 400, swing = 'swing', function () {})
+    }
   }
 
   $('.btn-mobile-menu').click(function () {
